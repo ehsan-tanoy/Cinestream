b30d8c3f7a5ed3f37b4f09acce8b0588f499dd9c
ehsan-tanoy <ehsanul.tanoy@gmail.com>
2026-05-12 18:33:41 +0600

    Update watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -72,4 +72,62 @@ function loadWatchlist() {
   });
 }
 
+const bottomNav = document.getElementById("bottomNav");
+
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+
+  const currentScroll = window.scrollY;
+
+  if (currentScroll > lastScrollY && currentScroll > 100) {
+    // scroll down
+    bottomNav.classList.add("hide");
+  } else {
+    // scroll up
+    bottomNav.classList.remove("hide");
+  }
+
+  lastScrollY = currentScroll;
+});
+
+loadWatchlist();
+      <img src="${IMG_URL + movie.poster}" />
+
+      <div class="badge left">
+  ${movie.type === "Movie" ? "Movie" : "Series"}
+</div>
+
+      <div class="badge right">${movie.rating?.toFixed(1)}</div>
+
+      <div class="overlay">
+        <h3>${movie.title || movie.name}</h3>
+        <p>${(movie.first_air_date || movie.year || "").split("-")[0]}</p>
+      </div>
+
+      <button class="remove-btn">  <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#f4f4f4"><path d="m480-240-168 72q-40 17-76-6.5T200-241v-519q0-33 23.5-56.5T280-840h400q33 0 56.5 23.5T760-760v519q0 43-36 66.5t-76 6.5l-168-72Z"/></svg>
+      </button>
+    `;
+
+    div.querySelector(".remove-btn").onclick = (e) => {
+      e.stopPropagation();
+      removeFromWatchlist(movie.id);
+    };
+
+    div.addEventListener("click", () => {
+
+  if (movie.type === "Movie") {
+    window.location.href = `details.html?id=${movie.id}`;
+  }
+
+  if (movie.type === "Series") {
+    window.location.href = `series-details.html?id=${movie.id}`;
+  }
+
+});
+
+    grid.appendChild(div);
+  });
+}
+
 loadWatchlist();
